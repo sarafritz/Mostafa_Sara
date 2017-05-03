484ac944f2365b7348f4183877bf78fd57d8f866
sarafritz <fritz@eeg.tuwien.ac.at>
2017-05-03 11:47:16 +0200

    Add files via upload

---
+++ src/CM/CM0xy_potDHAreas/main.py
@@ -0,0 +1,26 @@
+'''
+Created on Apr 20, 2017
+
+@author: simulant
+'''
+import numpy as np
+
+
+import AD.available_data  as AD_ 
+ 
+def potDHAreas(heat_density_map,pix_threshold,DH_threshold, minSizeObj=3,minSizeHole=5, bridge = square(2), conn=2):
+    return
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    
+
+    potDHAreas(AD_.heat_density_map,0.1,10, 4,5)
+    
